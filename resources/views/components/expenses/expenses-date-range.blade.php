69e77dda85466281f7fb9d2ee7a6e35c494e51ac
kevinpauneljohn <johnkevinpaunel@gmail.com>
2024-05-21 10:56:00 +0800

    set expenses date range by current month

--- resources/views/components/expenses/expenses-date-range.blade.php
+++ resources/views/components/expenses/expenses-date-range.blade.php
@@ -6,7 +6,7 @@
                         <i class="far fa-calendar-alt"></i>
                       </span>
         </div>
-        <input type="text" name="date" class="form-control float-right" id="reservation">
+        <input type="text" name="date" class="form-control float-right" id="expenses-date-range">
     </div>
     <!-- /.input group -->
 </div>
@@ -20,9 +20,12 @@
     <script>
         $(document).ready(function(){
             //Date range picker
-            $('#reservation').daterangepicker()
+            $('#expenses-date-range').daterangepicker()
+            setTimeout(function(){
+                $('#expenses-date-range').val('{{now()->startOfMonth()->format('m/d/Y')}} - {{now()->endOfMonth()->format('m/d/Y')}}').change();
+            },50)
         });
-        $(document).on('change','#reservation',function(){
+        $(document).on('change','#expenses-date-range',function(){
             let date = $(this).val();
 
             $.ajax({
