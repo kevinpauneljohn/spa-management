ece0c4d5c598ea2b8f3e29ca866a9d97d803f5bf
kevinpauneljohn <johnkevinpaunel@gmail.com>
2024-05-24 17:12:25 +0800

    inventories

--- resources/views/components/inventory/inventory-management.blade.php
+++ resources/views/components/inventory/inventory-management.blade.php
@@ -11,7 +11,7 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-12 table-responsive">
         <table id="inventory-list" class="table table-bordered table-hover" role="grid" style="width:100%;">
             <thead>
             <tr role="row">
