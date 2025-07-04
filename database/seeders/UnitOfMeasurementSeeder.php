7580db2c414e61a891adcc9b1c69b33d2bb2f76b
john kevin paunel <johnkevinpaunel@gmail.com>
2025-07-05 04:06:39 +0800

    unit of measurement seeder

--- database/seeders/UnitOfMeasurementSeeder.php
+++ database/seeders/UnitOfMeasurementSeeder.php
@@ -16,21 +16,21 @@ class UnitOfMeasurementSeeder extends Seeder
     public function run()
     {
         UnitOfMeasurement::insert([
-//            ['singular' => 'meter', 'plural' => 'meters', 'created_at' => now(), 'updated_at' => now()],
-//            ['singular' => 'centimeter', 'plural' => 'centimeters', 'created_at' => now(), 'updated_at' => now()],
-//            ['singular' => 'inch', 'plural' => 'inches', 'created_at' => now(), 'updated_at' => now()],
-//            ['singular' => 'millimeter', 'plural' => 'millimeters', 'created_at' => now(), 'updated_at' => now()],
-//            ['singular' => 'foot', 'plural' => 'feet', 'created_at' => now(), 'updated_at' => now()],
-//            ['singular' => 'liter', 'plural' => 'liters', 'created_at' => now(), 'updated_at' => now()],
-//            ['singular' => 'gallon', 'plural' => 'gallons', 'created_at' => now(), 'updated_at' => now()],
-//            ['singular' => 'piece', 'plural' => 'pieces', 'created_at' => now(), 'updated_at' => now()],
-//            ['singular' => 'ounce', 'plural' => 'ounces', 'created_at' => now(), 'updated_at' => now()],
+            ['singular' => 'meter', 'plural' => 'meters', 'created_at' => now(), 'updated_at' => now()],
+            ['singular' => 'centimeter', 'plural' => 'centimeters', 'created_at' => now(), 'updated_at' => now()],
+            ['singular' => 'inch', 'plural' => 'inches', 'created_at' => now(), 'updated_at' => now()],
+            ['singular' => 'millimeter', 'plural' => 'millimeters', 'created_at' => now(), 'updated_at' => now()],
+            ['singular' => 'foot', 'plural' => 'feet', 'created_at' => now(), 'updated_at' => now()],
+            ['singular' => 'liter', 'plural' => 'liters', 'created_at' => now(), 'updated_at' => now()],
+            ['singular' => 'gallon', 'plural' => 'gallons', 'created_at' => now(), 'updated_at' => now()],
+            ['singular' => 'piece', 'plural' => 'pieces', 'created_at' => now(), 'updated_at' => now()],
+            ['singular' => 'ounce', 'plural' => 'ounces', 'created_at' => now(), 'updated_at' => now()],
             ['singular' => 'bottle', 'plural' => 'bottles', 'created_at' => now(), 'updated_at' => now()],
         ]);
 
-//        Permission::create(['name' => 'add measurement'])->assignRole('owner');
-//        Permission::create(['name' => 'view measurement'])->assignRole('owner');
-//        Permission::create(['name' => 'edit measurement'])->assignRole('owner');
-//        Permission::create(['name' => 'delete measurement'])->assignRole('owner');
+        Permission::create(['name' => 'add measurement'])->assignRole('owner');
+        Permission::create(['name' => 'view measurement'])->assignRole('owner');
+        Permission::create(['name' => 'edit measurement'])->assignRole('owner');
+        Permission::create(['name' => 'delete measurement'])->assignRole('owner');
     }
 }
